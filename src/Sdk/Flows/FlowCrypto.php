e953c9ecece6e5477a3d4be1182c1a1aac8be624
Yvan Ngalle <nycorpdev@gmail.com>
2025-10-21 17:42:51 +0100

    adding message interactive list

---
+++ src/Sdk/Flows/FlowCrypto.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace NyCorp\Shortext\Sdk\Flows;
+
+use Exception;
+use phpseclib3\Crypt\AES;
+use phpseclib3\Crypt\RSA;
+
+class FlowCrypto
+{
+    public function decryptRequest(array $body, string $privatePem): FlowCryptoData
+    {
+        $encryptedAesKey = base64_decode($body['encrypted_aes_key'], true);
+        $encryptedFlowData = base64_decode($body['encrypted_flow_data']);
+        $initialVector = base64_decode($body['initial_vector']);
+
+        // Decrypt the AES key created by the client
+        $rsa = RSA::load($privatePem)
+            ->withPadding(RSA::ENCRYPTION_OAEP)
+            ->withHash('sha256')
+            ->withMGFHash('sha256');
+
+        try {
+            $decryptedAesKey = $rsa->decrypt($encryptedAesKey);
+        } catch (\Throwable $e) {
+            throw new Exception("<strong> RSA decrypt error: {$e->getMessage()} </strong>");
+        }
+
+        if ($decryptedAesKey === false) {
+            throw new Exception('RSA decrypt returned false. Check OAEP hash and padding.');
+        }
+
+        // Decrypt the Flow data
+        $aes = new AES('gcm');
+        $aes->setKey($decryptedAesKey);
+        $aes->setNonce($initialVector);
+        $tagLength = 16;
+        $encryptedFlowDataBody = substr($encryptedFlowData, 0, -$tagLength);
+        $encryptedFlowDataTag = substr($encryptedFlowData, -$tagLength);
+        $aes->setTag($encryptedFlowDataTag);
+
+        $decrypted = $aes->decrypt($encryptedFlowDataBody);
+        if (!$decrypted) {
+            throw new Exception('Decryption of flow data failed.');
+        }
+
+        return new FlowCryptoData(json_decode($decrypted, true), $decryptedAesKey, $initialVector);
+    }
+
+    public function encryptResponse($response, $aesKeyBuffer, $initialVectorBuffer): string
+    {
+        // Flip the initialization vector
+        $flipped_iv = ~$initialVectorBuffer;
+
+        // Encrypt the response data
+        $cipher = openssl_encrypt(json_encode($response), 'aes-128-gcm', $aesKeyBuffer, OPENSSL_RAW_DATA, $flipped_iv, $tag);
+        return base64_encode($cipher . $tag);
+    }
+}
---
+++ src/Sdk/Flows/FlowCryptoData.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace NyCorp\Shortext\Sdk\Flows;
+
+class FlowCryptoData
+{
+    private array $decryptedBody = [];
+    private string $aesKeyBuffer;
+    private string $initialVectorBuffer;
+
+    /**
+     * @param array $decryptedBody
+     * @param string $aesKeyBuffer
+     * @param string $initialVectorBuffer
+     */
+    public function __construct(array $decryptedBody, string $aesKeyBuffer, string $initialVectorBuffer)
+    {
+        $this->decryptedBody = $decryptedBody;
+        $this->aesKeyBuffer = $aesKeyBuffer;
+        $this->initialVectorBuffer = $initialVectorBuffer;
+    }
+
+    public function getDecryptedBody(): array
+    {
+        return $this->decryptedBody;
+    }
+
+    public function getAesKeyBuffer(): string
+    {
+        return $this->aesKeyBuffer;
+    }
+
+    public function getInitialVectorBuffer(): string
+    {
+        return $this->initialVectorBuffer;
+    }
+}
